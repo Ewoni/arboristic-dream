9064810763ca4b3c71e1f01db569f3aeef52d841
Ewoni <pankratov.050514@gmail.com>
2023-05-24 00:23:35 +0300

    Айди для кнопок. Тестовые функции при их нажатии.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,14 @@ if (window.jQuery) {
 else {
     console.log("Nothing");
 }
+
+function infoBtnOne() {
+    $("#info-button-1").slideUp()
+}
+
+function infoBtnTwo() {
+    $("#info-button-2").slideUp()
+}
+function infoBtnThree() {
+    $("#info-button-3").slideUp()
+}
